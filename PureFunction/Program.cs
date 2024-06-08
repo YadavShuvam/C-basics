ae4b342f41081565d47244a04c9377a6434c0c12
Shuvam Yadav <shuvam24@outlook.com>
2024-06-08 10:38:30 +0545

    asdasd

---
+++ PureFunction/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PureFunction
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int result = Subtract(10, 5);
+            Console.WriteLine(result); 
+
+            int result2 = Subtract(10, 5);
+            Console.WriteLine(result2); 
+        }
+
+       
+        static int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+}
